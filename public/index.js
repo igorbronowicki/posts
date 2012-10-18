436afce50e1ecd0fbc6e3daacc5ce0d3186edc63
Igor Bronowicki <igor.bronowicki@gmail.com>
2012-10-18 20:25:26 +0300

    add view

--- public/index.js
+++ public/index.js
@@ -2,9 +2,18 @@
 var PostModel = Backbone.Model.extend({
     defaults: {
         "title": "",
-        "desc": ""
+        "desc": "",
+        "time": "20 July 2012"
     },
-    url: "/posts/add"
+    url: "/posts/add",
+    initialize: function(){
+        //console.log("Instance of PostModel was created.");
+        //console.log(this);
+    },
+    validate: function(attributes){
+        //console.log("Try to validate.");
+        //console.log(attributes);
+    }
 });
 
 
@@ -15,19 +24,40 @@ var PostCollection = Backbone.Collection.extend({
 });
 
 
+$(function(){
+    // View для статей
+    var PostsView = Backbone.View.extend({
+        el: $('#posts'),
+        render: function(){
+            var template = $('#PostTemplate').html();
+            var context = this.model.toJSON();
+            //this.el.html(Mustache.render(template, context));
+
+            var templateArray = $('#PostArrayTemplate').html();
+            var contextArray = {list: this.collection.toJSON()};
+            this.el.html(Mustache.render(templateArray, contextArray));
+
+            return this; //recommended as this enables calls to be chained.
+        }
+    });
+
+    var posts = new PostCollection;
+    posts.fetch({
+        success: function(){
+            var postsView = new PostsView({model: posts.getByCid("c4"), collection: posts});
+            postsView.render();
+        },
+        error: function(){ console.log("Some shit happens."); }
+    });
+});
 
 
 
 
 
-var posts = new PostCollection;
+
 
 /*
 var a = new PostModel({"title":"ololo", "desc":"trololo"});
 a.save();
-
-posts.fetch({
-    success: function(){ console.log("suc00"); },
-    error: function(){ console.log("err00"); }
-});
 */
