212ad67335a8426b5b7c608a60cebd1a6d0c3d79
Igor Bronowicki <igor.bronowicki@gmail.com>
2012-10-19 14:28:29 +0300

    add css, posts order in DB and try to bind event to collection

--- public/index.js
+++ public/index.js
@@ -37,11 +37,16 @@ $(function(){
             var contextArray = {list: this.collection.toJSON()};
             this.el.html(Mustache.render(templateArray, contextArray));
 
+            console.log("PostsView was rendered one more time.");
             return this; //recommended as this enables calls to be chained.
-        }
+        },
+        initialize: function () { // Подписка на событие обновления коллекции
+            //TODO: this.model.bind('change', this.render, this);
+            //TODO: this.collection.bind('change', this.render, this);
+        },
     });
 
-    var posts = new PostCollection;
+    window.posts = new PostCollection;
     posts.fetch({
         success: function(){
             var postsView = new PostsView({model: posts.getByCid("c4"), collection: posts});
